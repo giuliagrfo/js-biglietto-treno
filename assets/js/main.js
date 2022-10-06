824e6a01d6abe6e1717314657bc33eff02270ac3
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-06 16:24:02 +0200

    discount under 18/ over 65

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,30 @@ const userAge = prompt('Quanti anni hai?');
 // Calcolare il prezzo totale del viaggio
 const priceOfKilometer = 0.21;
 
-const ticketPrice = kilometres * 0.21 ;
+const ticketPrice = kilometres * 0.21;
 
-document.getElementById('price').innerHTML = `From Padova to Venice the price is: ${ticketPrice} $` ;
+document.getElementById('price').innerHTML = `From Padova to Venice the price is: ${ticketPrice} $`;
+
+// Applicare uno sconto del 20% per i minorenni
+const percentageUnder18= 20;
+
+const discountValue18 = (ticketPrice / 100) * percentageUnder18; 
+
+const finalPrice18 = ticketPrice - discountValue18;
+
+
+// Applicare uno sconto del 40% per gli over 65
+const percentageOver65= 40;
+
+const discountValue65 = (ticketPrice / 100) * percentageOver65; 
+
+const finalPrice65 = ticketPrice - discountValue65;
+
+if (userAge < 18) {
+    document.getElementById('price').innerHTML = `From Padova to Venice the price is: ${finalPrice18} $`;
+} else if (userAge > 65) {
+    document.getElementById('price').innerHTML = `From Padova to Venice the price is: ${finalPrice65} $`;
+}
+
+
+
